aeff8a31f81741f66ae051d77145b5eabdf5a461
Recai Oktaş <roktas@bil.omu.edu.tr>
2012-03-21 14:29:33 +0200

    global görevler ekle

---
+++ _scripts/global.rb
@@ -0,0 +1,12 @@
+# Encoding: utf-8
+# ------------------------------------------------------------------------------
+# Global Görevler
+# ------------------------------------------------------------------------------
+
+GIT_AUTO_COMMIT_MESSAGE = '.'
+
+desc "'#{GIT_AUTO_COMMIT_MESSAGE}' değişiklik iletisiyle kaydet ve gönder"
+task :submit do
+  %x(git commit -a -m #{GIT_AUTO_COMMIT_MESSAGE})
+  %x(git push)
+end
